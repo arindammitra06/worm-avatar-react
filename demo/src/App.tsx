51226a3acaf2d7f0e847d11b801af0f979bbfb43
Arindam Mac <arindammitra06@gmail.com>
2023-09-06 09:52:57 -0400

    Update App.tsx

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WormAvatar, { hexColor, Expression, TongueOrTeeth } from 'worm-avatar-react';
 import { BlockPicker } from 'react-color';
